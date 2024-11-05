a1383f08e4a274e0a71e3ae93260e4ce5365c516
ManuelFSaucedo <manuelfsv20@gmail.com>
2024-11-05 01:38:26 -0600

    Agrege un detalle y una correccion

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-car_data = pd.read_csv('nothebooks/vehicles_us.csv')  # leer los datos
+car_data = pd.read_csv('notebooks/vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
+
+car_data
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
